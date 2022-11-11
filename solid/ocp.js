1bb46d03b3f882bb381b21143508c4192b9e39e8
mikekidby <mkidby@wiley.com>
2022-11-11 17:19:27 -0600

    init srp

--- solid/ocp.js
+++ solid/ocp.js
@@ -1,3 +1,11 @@
+//The open-closed principle (OCP) states that software entities (classes, modules, functions, etc.) should be open for extension, but closed for modification.
+//OCP is two-direction principle.
+
+// It is open for extension — This means you can extend what the module can do.
+// It is closed for modification — This means you cannot change the source code, even though you can extend the behavior of a module or entity.
+
+
+// NEVER MODIFY GLOBAL SOURCE CODE!!
 const iceCreamFlavors = ['chocolate', 'vanilla'];
 
 class makeIceCream {
@@ -12,6 +20,9 @@ class makeIceCream {
         }
     }
 }
+
+
+// addIceCream – to handle addition to the iceCreamFlavors array using the add() method. This means your code is closed to modification but open to an extension because you can add new flavors without directly affecting the array.
 
 class addIceCream {
     constructor(flavor) {
---
+++ solid/srp.js
@@ -0,0 +1,49 @@
+// Demonstrating Single Responsibility Principle
+
+// ValidatePerson class will only be responsible for validating a user, as seen below:
+class ValidatePerson {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    ValidateName(name) {
+        if (name.length > 3) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    ValidateAge(age) {
+        if (age > 18) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+
+// DisplayPerson will be responsible for displaying a person, as you can see in the code block below:
+class DisplayPerson {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+        this.validate = new ValidatePerson(this.name, this.age);
+    }
+
+    Display() {
+        if (
+            this.validate.ValidateName(this.name) &&
+            this.validate.ValidateAge(this.age)
+        ) {
+            console.log(`Name: ${this.name} and Age: ${this.age}`);
+        } else {
+            console.log('Invalid');
+        }
+    }
+}
+
+
+//Our classes have just one reason to change. If you want to change the DisplayPerson class, it won’t affect the ValidatePerson class.
